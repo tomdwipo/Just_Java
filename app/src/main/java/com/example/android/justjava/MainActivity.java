1cc419d068a1bc90245b782c70cc00abc1b512d9
tommyputranto <tommyputranto1@gmail.com>
2015-07-14 19:07:15 +0700

    update 13 xml order summary dihapus update java

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -136,9 +136,6 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method displays the given text on the screen.
      */
-    private void displayMessage(String message) {
-        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText(message);
-    }
+
 
 }
